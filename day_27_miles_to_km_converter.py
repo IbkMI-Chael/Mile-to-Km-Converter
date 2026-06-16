8aedb1d60caef47e546ae27f11b8f5e2b99c7da6
Michael I. Arebisola <153301546+IbkMI-Chael@users.noreply.github.com>
2026-06-16 14:50:48 +0100

    Add files via upload

---
+++ day_27_miles_to_km_converter.py
@@ -0,0 +1,75 @@
+# from tkinter import *
+#
+#
+# def btn_clicked():
+# 	user_input = input.get()
+# 	my_label.config(text = user_input)
+#
+#
+# # Creating a window
+# window = Tk()
+# window.title("My first GUI program")
+# window.minsize(width = 500, height = 350)
+# window.config(padx = 100, pady = 200)
+#
+# # Label
+# my_label = Label(text = "I Am A Label", font = ("arial", 24, "bold"))
+# my_label.config(text = "New text")
+# my_label.grid(column = 0, row = 0)
+# my_label.config(padx = 50, pady = 50)
+#
+# # Button
+# button = Button(text = "Click me!", command = btn_clicked)
+# button.grid(column = 1, row = 1)
+#
+# # New button
+# btn2 = Button(text = "Heya!", command = btn_clicked)
+# btn2.grid(column = 2, row = 0)
+#
+# # Entry
+# input = Entry(width = 20)
+# input.grid(column = 3, row = 2)
+#
+# ##
+# ##
+# ##
+# window.mainloop()
+
+
+from tkinter import *
+
+
+def convert():
+	"""Converts miles to kilometre"""
+	km = float(miles_input.get()) * 1.609  # Get entry input, convert to int and multiply
+	km_result.config(text = f"{km}")
+
+
+# Creating window
+window = Tk()
+window.minsize(width = 300, height = 150)  # Specifying window size
+window.title("Mile to Km Converter")  # Window title
+window.config(padx = 20, pady = 20)  # Specifying padding
+
+# Entry
+miles_input = Entry(width = 15)
+miles_input.grid(column = 1, row = 0)
+
+# Labels
+miles_label = Label(text = "Miles", font = ("arial", 15, "bold"))
+miles_label.grid(row = 0, column = 2)
+is_equal_label = Label(text = "is equal to", font = ("arial", 15, "bold"))
+is_equal_label.grid(column = 0, row = 1)
+km_result = Label(text = 0, font = ("arial", 15, "bold"))
+km_result.grid(column = 1, row = 1)
+km_label = Label(text = "Km", font = ("arial", 15, "bold"))
+km_label.grid(column = 2, row = 1)
+
+# Button
+button = Button(text = "Calculate", command = convert)
+button.grid(column = 1, row = 2)
+
+##
+##
+##
+window.mainloop()
